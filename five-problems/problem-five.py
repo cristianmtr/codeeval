8fea945035ee242f95d7aa7e116a93bea4fc4241
cristian <cristi.mtr@gmail.com>
2015-05-09 15:27:51 +0200

    problem five solved

--- five-problems/problem-five.py
+++ five-problems/problem-five.py
@@ -7,28 +7,34 @@
 # For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
 from itertools import product as cartesianproduct
 
+
 def main():
-    cases = [list(case) for case in list(cartesianproduct([' ','-','+'],repeat=8))]
+    cases = [list(case) for case in list\
+             (cartesianproduct([' ','-','+'],repeat=8))]
     print 'total nr of cases = {}'.format(len(cases))
+    solutions = 0
     for case in cases:
-        # print "CASE = {}".format(case)
-        # pdb.set_trace()
+        # first we need to combine the numbers that have
+        # blanks between them
         numbers = [1,2,3,4,5,6,7,8,9]
-        i = 0        
+        while " " in case:
+            idx = case.index(" ")
+            numbers[idx] = numbers[idx] * 10 + numbers.pop(idx+1)
+            case.pop(idx)
+        i = 0
         while i < len(case):
             symbol = case[i]
-            if symbol == ' ':
-                numbers[0] = numbers[0] * 10 + numbers.pop(1)
             if symbol == '-':
                 numbers[0] = numbers[0] - numbers.pop(1)
             if symbol == '+':
                 numbers[0] = numbers[0] + numbers.pop(1)
             i += 1
         if numbers[0] == 100:
-            print numbers
-            print 'found case: {}'.format(case)
+            solutions += 1
+            print 'found case: {}'.format(''.join(case))
+    print 'solutions = {}'.format(solutions)
     return
-                
+
 
 if __name__ == "__main__":
     main()
