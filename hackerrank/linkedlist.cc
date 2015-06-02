877ff8001951ac24995ff218df04d87d0500b565
cristian mtr <cristi.mtr@gmail.com>
2015-06-02 20:56:21 +0200

    linkedlist w/ Insert_at_index()

--- hackerrank/linkedlist.cc
+++ hackerrank/linkedlist.cc
@@ -2,6 +2,7 @@
 // Insert - insert element at end
 // Print - print all elements from head to end
 // Insert_at_head - insert element as new head
+// Insert_at_index - insert new element at given index
 // Takes input nr of nodes to create followed
 // by the data each of them should contain
 // Prints the data
@@ -62,6 +63,32 @@ Node* Insert_at_head(Node *head, int data)
   return newNode;
 }
 
+Node* Insert_at_index(Node *head, int data, int position)
+{
+  int i = 0;
+  Node *followerNode = new Node;
+  followerNode = head;
+  Node *previousNode = new Node;
+  while (i != position )
+    {
+      previousNode = followerNode;
+      followerNode = followerNode->next;
+      i++;
+    }
+  Node *newNode = new Node;
+  newNode->data = data;
+  newNode->next = followerNode;
+  previousNode->next = newNode;
+  if (i == 0)
+    {
+      return newNode;
+    }
+  else
+    {
+      return head;
+    }
+}
+
 int main()
 {
   using namespace std;
@@ -73,8 +100,13 @@ int main()
     {
       head = Insert(head,data);
     }
+  cout << "contents after reading from stdin:" << endl;
   Print(head);
   head = Insert_at_head(head, 99);
+  cout << "contents after inserting at head: " << endl;
+  Print(head);
+  head = Insert_at_index(head, 2301, 2);
+  cout << "contents after inserting at index: " << endl;
   Print(head);
   return 0;
 }
